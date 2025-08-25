b628171fb079e1630702fd2df05f0ed1e0a8011a
Alex Amedia <aamedia@asu.edu>
2025-08-25 11:15:14 -0700

    Java Collections: Framework, Lists, Sets, Maps,

---
+++ Hashing/src/dev/lpa/PlayingCard.java
@@ -0,0 +1,39 @@
+package dev.lpa;
+
+public class PlayingCard {
+
+    private String suit;
+    private String face;
+    private int internalHash;
+
+    public PlayingCard(String suit, String face) {
+        this.suit = suit;
+        this.face = face;
+        this.internalHash = (suit.equals("Hearts")) ? 11 : 12;
+    }
+
+    @Override
+    public String toString() {
+        return face + " of " + suit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return false;
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//
+//        PlayingCard that = (PlayingCard) o;
+//
+//        if (!suit.equals(that.suit)) return false;
+//        return face.equals(that.face);
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+//        int result = suit.hashCode();
+//        result = 31 * result + face.hashCode();
+//        return result;
+    }
+}
